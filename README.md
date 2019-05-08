b9cab2fabee0ee91dbce741deafa9390e723e168
Wendel Santos <henrique.santos1988@gmail.com>
2019-05-08 13:47:44 -0300

    armazenando projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+# Calculadora
+Calculadora para celular criada usando react native.
+
+## Installation
+clone o repositorio 
+
+```bash
+https://github.com/ricksantos88/calculator_react-native.git
+```
+
+## Usage
+```python
+npm i -g react-native-cli
+npm i 
+react-native run-android
+```
---
+++ src/components/Button.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableHighlight
+} from 'react-native'
+import { black } from 'ansi-colors';
+
+const styles = StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888',
+        color: '#000000'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3,
+    },
+})
+
+export default props => {
+    const styleButton = [styles.button]
+    if (props.double) styleButton.push(styles.buttonDouble)
+    if (props.triple) styleButton.push(styles.buttonTriple)
+    if (props.operation) styleButton.push(styles.operationButton)
+
+    return(
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={styleButton}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
